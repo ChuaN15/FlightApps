a1af022486019dd45b489691c4a1624b24bf0dab
CHUAN CHUAN YOU <b031720029@student.utem.edu.my>
2022-09-03 15:59:22 +0800

    Services update

---
+++ FlightApps/FlightApps/Services/LoginService.cs
@@ -0,0 +1,38 @@
+﻿using FlightApps.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApps.Services
+{
+    public class LoginService
+    {
+        HttpClient client = new HttpClient();
+        //Post
+        public async Task<List<User>> RegisterUser(User item)
+        {
+            List<User> Items = new List<User>();
+            var urlGetDashboard = "https://6ff8-2001-f40-904-4829-b054-21cb-c957-48f1.ngrok.io/Home/getAssetList";
+
+            Uri uri = new Uri(string.Format(urlGetDashboard, string.Empty));
+
+            string json = JsonConvert.SerializeObject(item);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await client.PostAsync(uri, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                Items = JsonConvert.DeserializeObject<List<User>>(data);
+                return Items;
+            }
+
+            return null;
+        }
+    }
+}
