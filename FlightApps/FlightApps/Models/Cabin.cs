bdf62d9d21ce4b0bab270d5ddf9bcf2bbb31e4fb
CHUAN CHUAN YOU <b031720029@student.utem.edu.my>
2022-09-04 12:48:57 +0800

    recommit

---
+++ FlightApps/FlightApps/Models/Cabin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightApps.Models
+{
+    public class Cabin
+    {
+        public Cabin(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
--- FlightApps/FlightApps/ViewModels/AboutViewModel.cs
+++ FlightApps/FlightApps/ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FlightApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -7,8 +10,16 @@ namespace FlightApps.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        public ObservableCollection<Cabin> CabinList { get; }
+
+        public Cabin SelectedCabin { get; }
         public AboutViewModel()
         {
+            CabinList = new ObservableCollection<Cabin>();
+            CabinList.Add(new Cabin(1, "Economy"));
+            CabinList.Add(new Cabin(2, "Business"));
+            CabinList.Add(new Cabin(3, "First Class"));
+
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
